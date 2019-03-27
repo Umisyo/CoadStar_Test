86301a9fe26d017e9be5530656c8c7e47fb5abc0
Umisyo <kusunokisouta@gmail.com>
2019-03-27 19:05:48 +0900

    C:/Program Files/Git/test

--- helloworld/application.py
+++ helloworld/application.py
@@ -1,4 +1,6 @@
 #!flask/bin/python
+# coading: utf-8
+
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
@@ -12,6 +14,12 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
+
+@application.route('/test', methods=['GET'])
+def test_get():
+    hoge:str = 'hoge'
+    return hoge 
+
 
 if __name__ == '__main__':
     flaskrun(application)
